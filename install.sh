9e5737041a50ea8a247c69a2673c5a0cd9415082
raffilua <75070942+raffilua@users.noreply.github.com>
2021-02-01 10:47:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,52 +2,38 @@
 cd $HOME/MODEKSA
 install() {
 rm -rf $HOME/.telegram-cli
-sudo chmod +x tg
-chmod +x MODEKSA
-chmod +x ts
-./ts
-}
-get() {
-rm -fr MODEDEV.lua
-rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/raffilua/MODEKSA/master/MODEDEV.lua"
-lua MODEKSA.lua
-}
-installall(){
-apt update
-apt upgrade
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install tmux
-sudo apt-get install luarocks
-sudo apt-get install screen
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get update
-sudo apt-get install
-sudo apt-get install upstart-sysv
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz;tar zxpf luarocks-2.2.2.tar.gz;cd luarocks-2.2.2 && ./configure
-sudo make bootstrap
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo luarocks install luasocket
 sudo luarocks install luasec
-sudo apt-get install libconfig++9v5 -y 
+sudo luarocks install redis-lua
+sudo luarocks install Lua-cURL
+sudo service redis-server start
+sudo apt-get update -y
+sudo apt-get install g++-4.7 -y c++-4.7
+sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y curl -y htop -y
+sudo apt-get install screen -y
 sudo apt-get install libstdc++6 -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-sudo apt-get install lua-lgi -y  
-sudo apt-get install libnotify-dev -y 
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-sudo apt-get update 
-sudo apt-get upgrade -y
-}
-if [ "$1" = "ins" ]; then
-install
-fi
-if [ "$1" = "get" ]; then
-get
-fi
-installall
-cd ..
-rm -rf luarocks*
-cd MODEKSA
-rm -rf luarocks*
+sudo apt-get install lua-lgi -y
+sudo apt-get install libnotify-dev -y
+sudo chmod +x tg
 lua MODEKSA.lua
-
+sudo chmod +x MODEKSA
+sudo chmod +x ./NG
+./NG
+}
+if [ "$1" = "install" ]; then
+install
+else
+if [ ! -f ./tg ]; then
+echo "Tg Is Not Found"
+exit 1
+fi
+sudo chmod +x tg
+lua MODEKSA.lua
+sudo chmod +x MODEKSA
+sudo chmod +x ./NG
+./NG
+fi
